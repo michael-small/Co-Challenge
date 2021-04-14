e48d06b7572381871ddcd0ecba5d613a223d9dce
michael-small <small203@morris.umn.edu>
2021-04-13 23:48:50 -0500

    Convert ratings to MUI Card Grid
    Addresses #28 (Convert Repos and Ratings into MUI card grid)
    Project: 'Breakdown `Cockpit` into subcomponents'

--- client/src/components/Ratings/Rating/Rating.js
+++ client/src/components/Ratings/Rating/Rating.js
@@ -4,18 +4,19 @@ import React from 'react';
 import axios from 'axios';
 import envs from '../../../envs/envs';
 
+import './Rating.scss';
+import Card from '@material-ui/core/Card';
+
 export default function Rating(props) {
 	async function deleteRating() {
 		await axios.delete(`${envs}/ratings/${props.id}`);
 		props.deleteRating();
 	}
 	return (
-		<div>
-			<div style={{ margin: '2px' }}>
-				<Typography variant='body1'>User: {props.user}</Typography>
-				<Typography variant='body1'>Rating: {props.rating}</Typography>
-				<Button onClick={deleteRating}>x</Button>
-			</div>
-		</div>
+		<Card style={{ height: '100%' }} className={'card'}>
+			<Typography variant='body1'>User: {props.user}</Typography>
+			<Typography variant='body1'>Rating: {props.rating}</Typography>
+			<Button onClick={deleteRating}>x</Button>
+		</Card>
 	);
 }
