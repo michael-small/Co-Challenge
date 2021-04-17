7074a2b8426bc971efbea6305eae23c9ab13ebbc
michael-small <small203@morris.umn.edu>
2021-04-16 23:40:12 -0500

    Fix comment in mongoose to be string, not number
    Project: 'Create `Comments` components'`

--- models/commentModel.js
+++ models/commentModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
 	user: { type: String },
-	comment: { type: Number },
+	comment: { type: String },
 });
 
 const Comment = mongoose.model('comment', commentSchema);
