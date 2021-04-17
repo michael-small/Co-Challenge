16886e6cfe75bf1ef004096458d8109e9943e162
michael-small <small203@morris.umn.edu>
2021-04-16 23:56:31 -0500

    Import styled-components, use on comment cards
    I think my linter is helping me avoid inline styles. It adds ()
    around the inner {} in an inline style. This broke the width
    declaration. I decided that it was time to pull in styled-components
    to save the day.
    
    Project: 'Create `Comments` components'

--- client/src/components/Comments/Comment/Comment.js
+++ client/src/components/Comments/Comment/Comment.js
@@ -16,6 +16,12 @@ import Avatar from '@material-ui/core/Avatar';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+import styled from 'styled-components';
+
+const StyledCard = styled(Card)`
+	max-width: 300px;
+	margin-bottom: 6px;
+`;
 
 export default function Comment(props) {
 	const [user, setUser] = [''];
@@ -41,7 +47,7 @@ export default function Comment(props) {
 	const TEMP_DEV_PHOTO =
 		'https://lh3.googleusercontent.com/fife/ABSRlIrLS8xWIB3GFNQ56m5yOW3uBZ6vtyw92EGwGXnSzPEztH8tHrTVK-srdB03wB0RCwzFdF4H7i0Cev5zt4MbVxCPyz-UR-hK10ahL07NB0aGH1oLaceB3mYCgBrpviKpVb1-Ic-FJcHVStl8_SX_R1Q3FxDtVun28JELbTl5KHfPHNYsJL3qOVtpm2K3O40NuNQpHawPmz6CaSZoIIAjowXaL3WC8K9A46ove6LHAtTfwp70WLG76PiuTOak5YJFsPEmdJHMBRZAGOk4m5HSGMXUvw2Abizecq5N2lqpo_DIwm1KR6FHYIkXK5o4OuDcvNSn-E3Bhxj1lbzmjpfOnaDV4yFUKfAP-EE1t0DbenFQvkvdcqSWCDpGQUBpPYK0nSKdr4yZp6wvyfLjYQPd9Rth4jqeVPlTn8eUTD7tq6jotHSW_kKL-HZCjsdmxH1akZ00CAYqMDY0I5jaBDNaLxntL3vMJzbf1gzzw4JCpBv3eq18qm9pnFEqzWbc2M4DLEkAQPtudx4K55rnrViftwwCI0hgePjb76bN4L0YXEiYK_L9ehuFiEaHdk5u5YZkj7rHTBFbSANHTsF6eazw5Shr9Cx3oGLY-o4HFlp1bLd8V2EPH_rZml-5AGFHTdgaPulQSaqa39spxmsrHEJJF_0po5aBc2nZAsyoyE6e0Ih-FzoPjlwJkuoZzb1DVP8OD9z4DU8r5r9dU8s6HoosP2KEmBZ9XvjYyhtvlxX3tb-LgGw=s32-c';
 	return (
-		<Card style={{ maxWidth: '300px' }}>
+		<StyledCard>
 			<CardHeader
 				avatar={<Avatar alt={user} src={TEMP_DEV_PHOTO} />}
 				action={
@@ -78,6 +84,6 @@ export default function Comment(props) {
 				title={props.user}
 			/>
 			<CardContent>{props.comment}</CardContent>
-		</Card>
+		</StyledCard>
 	);
 }
